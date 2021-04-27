e4d56251a8302b816f023b36f8e85500463f42aa
Norfarisha Jefree <72587541+siseisya@users.noreply.github.com>
2021-04-27 22:04:04 +0800

    Update apply-book.php

--- apply-book.php
+++ apply-book.php
@@ -70,6 +70,7 @@ if (strlen($_SESSION['emplogin']) == 0) {
         <link href="alpha.css" rel="stylesheet" type="text/css" />
         <link href="table.css" rel="stylesheet" type="text/css" />
         <link href="background.css" rel="stylesheet" type="text/css" />
+        <link href="apply.css" rel="stylesheet" type="text/css" />
 
 
         <!-- Title -->
@@ -83,10 +84,11 @@ if (strlen($_SESSION['emplogin']) == 0) {
 
     </head>
 
+
     <body>
         <?php include('includes/header.php'); ?>
 
-    
+
 
         <div style="margin-left:15%">
             <div class="w3-container">
@@ -95,94 +97,134 @@ if (strlen($_SESSION['emplogin']) == 0) {
                     <div class="leftcolumn" style="background-color:white;">
                         <div class="card">
                             <h4> <b>Borang UUM/SAC/11</b></h4>
-                            
-                                <form id="example-form" enctype="multipart/form-data" method="post" name="addemp">
 
-                                    <table id="applybook" align="center">
+                            <form id="example-form" enctype="multipart/form-data" method="post" name="addemp">
 
-                                        <tbody>
+                                <table id="applybook" align="center">
 
-                                            <?php if ($error) { ?><div class="errorWrap"><strong>ERROR </strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
+                                    <tbody>
 
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> Select Vehicle :</b> </td>
-                                                <td>
-                                                    <select name="vehicle" autocomplete="off">
-                                                        <option value="">Select vehicle type...</option>
-                                                        <?php $sql = "SELECT vehicle from vehicletype";
-                                                        $query = $dbh->prepare($sql);
-                                                        $query->execute();
-                                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                                        $cnt = 1;
-                                                        if ($query->rowCount() > 0) {
-                                                            foreach ($results as $result) {   ?>
-                                                                <option value="<?php echo htmlentities($result->vehicle); ?>"><?php echo htmlentities($result->vehicle); ?></option>
-                                                        <?php }
-                                                        } ?>
-                                                    </select>
-                                                    </select>
-                                                </td>
+                                        <?php if ($error) { ?><div class="errorWrap"><strong>ERROR </strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
 
-
-                                            </tr>
-
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> From Date :</b> </td>
-                                                <td>
-                                                    <input placeholder="" id="mask1" name="fromdate" type="date" data-inputmask="'alias': 'date'" required>
-                                                </td>
-                                            </tr>
-
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> To Date :</b> </td>
-                                                <td>
-                                                    <input placeholder="" id="mask1" name="todate" type="date" data-inputmask="'alias': 'date'" required>
-                                                </td>
-                                            </tr>
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> Select Vehicle :</b> </td>
+                                            <td>
+                                                <select name="vehicle" autocomplete="off">
+                                                    <option value="">Select vehicle type...</option>
+                                                    <?php $sql = "SELECT vehicle from vehicletype";
+                                                    $query = $dbh->prepare($sql);
+                                                    $query->execute();
+                                                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                                    $cnt = 1;
+                                                    if ($query->rowCount() > 0) {
+                                                        foreach ($results as $result) {   ?>
+                                                            <option value="<?php echo htmlentities($result->vehicle); ?>"><?php echo htmlentities($result->vehicle); ?></option>
+                                                    <?php }
+                                                    } ?>
+                                                </select>
+                                                </select>
+                                            </td>
 
 
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> From Time:</b> </td>
-                                                <td>
-                                                    <input id="textarea1" name="fromtime" type="text" length="100" required>
-                                                </td>
-                                            </tr>
+                                        </tr>
 
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> To Time:</b> </td>
-                                                <td>
-                                                    <input id="textarea2" name="totime" type="text" length="100" required>
-                                                </td>
-                                            </tr>
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> From Date :</b> </td>
+                                            <td>
+                                                <input placeholder="" id="mask1" name="fromdate" type="date" data-inputmask="'alias': 'date'" required>
+                                            </td>
+                                        </tr>
 
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> Destination:</b> </td>
-                                                <td>
-                                                    <input id="textarea3" name="destination" type="text" length="500" required>
-                                                </td>
-                                            </tr>
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> To Date :</b> </td>
+                                            <td>
+                                                <input placeholder="" id="mask1" name="todate" type="date" data-inputmask="'alias': 'date'" required>
+                                            </td>
+                                        </tr>
 
-                                            <tr>
-                                                <td style="font-size:16px;"> <b> Programme:</b> </td>
-                                                <td>
-                                                    <input id="textarea4" name="programme" type="text" required>
-                                                </td>
-                                            </tr>
 
-                                            <tr>
-                                                <td style="font-size:16px;"> <b>File Upload:</b> </td>
-                                                <td>
-                                                    <input type="file" name="file[]" id="file" multiple>
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> From Time:</b> </td>
+                                            <td>
+                                                <input id="textarea1" name="fromtime" type="text" length="100" required>
+                                            </td>
+                                        </tr>
 
-                                                    <a download="<?php echo $files[$a] ?>" href="uploads/<?php echo $files[$a] ?>"><?php echo $files[$a] ?></a>
-                                                </td>
-                                            </tr>
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> To Time:</b> </td>
+                                            <td>
+                                                <input id="textarea2" name="totime" type="text" length="100" required>
+                                            </td>
+                                        </tr>
 
-                                        </tbody>
-                                    </table>
-                                    <button type="submit" name="apply" id="apply" align="center" class="button3">Apply</button>
-                                </form>
-                           
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> Destination:</b> </td>
+                                            <td>
+                                                <input id="textarea3" name="destination" type="text" length="500" required>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td style="font-size:16px;"> <b> Programme:</b> </td>
+                                            <td>
+                                                <input id="textarea4" name="programme" type="text" required>
+                                            </td>
+                                        </tr>
+
+
+                                    </tbody>
+                                </table>
+
+
+                                <div class="row it">
+                                    <div class="col-sm-offset-1 col-sm-10" id="one">
+                                        <p>
+                                            Please upload documents only in 'pdf', 'docx', 'rtf', 'jpg', 'jpeg', 'png' & 'text' format.
+                                        </p><br>
+                                        <div class="row">
+                                            <div class="col-sm-offset-4 col-sm-4 form-group">
+                                                <h3 class="text-center">My Documents</h3>
+                                            </div>
+                                            <!--form-group-->
+                                        </div>
+                                        <!--row-->
+                                        <div id="uploader">
+                                            <div class="row uploadDoc">
+                                                <div class="col-sm-3">
+                                                    <div class="docErr">Please upload valid file</div>
+                                                    <!--error-->
+                                                    <div class="fileUpload btn btn-orange">
+                                                        <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon">
+                                                        <span class="upl" id="upload">Upload document</span>
+                                                        <input type="file" class="upload up" id="up" onchange="readURL(this);" name="file[]" id="file" multiple />
+
+
+                                                        <a download="<?php echo $files[$a] ?>" href="uploads/<?php echo $files[$a] ?>"><?php echo $files[$a] ?></a>
+                                                    </div><!-- btn-orange -->
+
+                                                </div><!-- col-3 -->
+
+
+                                            </div>
+                                            <!--row-->
+                                        </div>
+                                        <!--uploader-->
+                                        <div class="text-center">
+                                            <a class="btn btn-new"><i class="fa fa-plus"></i> Add new</a>
+        
+
+                                            <button type="submit" name="apply" id="apply" align="center" class="btn btn-next"><i class="fa fa-paper-plane">Apply</button>
+                                        </div>
+                                    </div>
+                                    <!--one-->
+                                </div><!-- row -->
+
+
+
+
+
+                            </form>
+
 
                         </div>
                     </div>
@@ -207,6 +249,72 @@ if (strlen($_SESSION['emplogin']) == 0) {
 
             </div>
             <?php include('calendar.php'); ?>
+
+            <script>
+                var fileTypes = ['pdf', 'docx', 'rtf', 'jpg', 'jpeg', 'png', 'txt']; //acceptable file types
+                function readURL(input) {
+                    if (input.files && input.files[0]) {
+                        var extension = input.files[0].name.split('.').pop().toLowerCase(), //file extension from input file
+                            isSuccess = fileTypes.indexOf(extension) > -1; //is extension in acceptable types
+
+                        if (isSuccess) { //yes
+                            var reader = new FileReader();
+                            reader.onload = function(e) {
+                                if (extension == 'pdf') {
+                                    $(input).closest('.fileUpload').find(".icon").attr('src', 'https://image.flaticon.com/icons/svg/179/179483.svg');
+                                } else if (extension == 'docx') {
+                                    $(input).closest('.fileUpload').find(".icon").attr('src', 'https://image.flaticon.com/icons/svg/281/281760.svg');
+                                } else if (extension == 'rtf') {
+                                    $(input).closest('.fileUpload').find(".icon").attr('src', 'https://image.flaticon.com/icons/svg/136/136539.svg');
+                                } else if (extension == 'png') {
+                                    $(input).closest('.fileUpload').find(".icon").attr('src', 'https://image.flaticon.com/icons/svg/136/136523.svg');
+                                } else if (extension == 'jpg' || extension == 'jpeg') {
+                                    $(input).closest('.fileUpload').find(".icon").attr('src', 'https://image.flaticon.com/icons/svg/136/136524.svg');
+                                } else if (extension == 'txt') {
+                                    $(input).closest('.fileUpload').find(".icon").attr('src', 'https://image.flaticon.com/icons/svg/136/136538.svg');
+                                } else {
+                                    //console.log('here=>'+$(input).closest('.uploadDoc').length);
+                                    $(input).closest('.uploadDoc').find(".docErr").slideUp('slow');
+                                }
+                            }
+
+                            reader.readAsDataURL(input.files[0]);
+                        } else {
+                            //console.log('here=>'+$(input).closest('.uploadDoc').find(".docErr").length);
+                            $(input).closest('.uploadDoc').find(".docErr").fadeIn();
+                            setTimeout(function() {
+                                $('.docErr').fadeOut('slow');
+                            }, 9000);
+                        }
+                    }
+                }
+                $(document).ready(function() {
+
+                    $(document).on('change', '.up', function() {
+                        var id = $(this).attr('id'); /* gets the filepath and filename from the input */
+                        var profilePicValue = $(this).val();
+                        var fileNameStart = profilePicValue.lastIndexOf('\\'); /* finds the end of the filepath */
+                        profilePicValue = profilePicValue.substr(fileNameStart + 1).substring(0, 20); /* isolates the filename */
+                        //var profilePicLabelText = $(".upl"); /* finds the label text */
+                        if (profilePicValue != '') {
+                            //console.log($(this).closest('.fileUpload').find('.upl').length);
+                            $(this).closest('.fileUpload').find('.upl').html(profilePicValue); /* changes the label text */
+                        }
+                    });
+
+                    $(".btn-new").on('click', function() {
+                        $("#uploader").append('<div class="row uploadDoc"><div class="col-sm-3"><div class="docErr">Please upload valid file</div><!--error--><div class="fileUpload btn btn-orange"> <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon"><span class="upl" id="upload">Upload document</span><input type="file" class="upload up" id="up" onchange="readURL(this);" /></div></div><div class="col-sm-8"><input type="text" class="form-control" name="" placeholder="Note"></div><div class="col-sm-1"><a class="btn-check"><i class="fa fa-times"></i></a></div></div>');
+                    });
+
+                    $(document).on("click", "a.btn-check", function() {
+                        if ($(".uploadDoc").length > 1) {
+                            $(this).closest(".uploadDoc").remove();
+                        } else {
+                            alert("You have to upload at least one document.");
+                        }
+                    });
+                });
+            </script>
     </body>
 
     </html>
