3cd6d2a8433b982ddfc409d2616a2176b651a167
Norfarisha Jefree <72587541+siseisya@users.noreply.github.com>
2021-05-18 15:23:30 +0800

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -14,6 +14,53 @@
   img {
     border-radius: 5px;
   }
+
+    /* Style the body */
+    body {
+      font-family: Arial;
+      margin: 0;
+    }
+
+    /* Header/Logo Title */
+    .header {
+      padding: 20px;
+      text-align: left;
+      background: black;
+      color: white;
+      font-size: 30px;
+    }
+
+    /* Page Content */
+    .content {
+      padding: 15px;
+    }
+
+    a.button3 {
+      display: inline-block;
+      padding: 1.5em 2.5em;
+      margin: 0 0.9em 0.9em 0;
+      border-radius: 2em;
+      box-sizing: border-box;
+      text-decoration: none;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 300;
+      color: #FFFFFF;
+      background-color: #4eb5f1;
+      text-align: center;
+      transition: all 0.2s;
+    }
+
+    a.button3:hover {
+      background-color: #4095c6;
+    }
+
+    @media all and (max-width:30em) {
+      a.button3 {
+        display: block;
+        margin: 0.2em auto;
+      }
+    }
+
 </style>
 
 <body>
@@ -27,6 +74,13 @@
 
     </div>
   </nav>
+
+  <a href="dashboard.php" class="button3">Dashboard</a>
+  <a href="manage-student.php" class="button3">Manages Students</a>
+
+  <a href="bookings.php" class="button3">List of Bus Booking Applications</a>
+
+  <a href="logout.php" class="button3">Logout</a>
 
   <div class="container">
     <h3>BBS | Admin Portal | Student Accommodation Centre (SAC)</h3>
