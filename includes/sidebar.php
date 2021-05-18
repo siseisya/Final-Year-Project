c4f15bae8a25c36ceb96baec1c8963cd621d5db0
Norfarisha Jefree <72587541+siseisya@users.noreply.github.com>
2021-05-18 15:32:37 +0800

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,29 +6,20 @@
   <div class="w3-sidebar w3-bar-block w3-dark-grey w3-animate-left" style="display:none" id="mySidebar">
     <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
 
+
     <div class="w3-dropdown-hover">
-      <button class="w3-button">Student Profile
+
+
+      <button class="w3-button">Bus Booking Management
         <i class="fa fa-caret-down"></i>
       </button>
       <div class="w3-dropdown-content w3-bar-block">
-        <a href="myprofile.php" class="w3-bar-item w3-button">My Profile</a>
-        <a href="myprofile1.php" class="w3-bar-item w3-button">My Profile1</a>
-        <a href="update-student1.php" class="w3-bar-item w3-button">Update Profile</a>
+        <a href="approvedbook-history.php" class="w3-bar-item w3-button">Approved Bus Booking </a>
+        <a href="pendingbook-history.php" class="w3-bar-item w3-button">Pending Bus Booking </a>
+        <a href="notapprovedbook-history.php" class="w3-bar-item w3-button">Rejected Bus Booking </a>
       </div>
     </div>
 
-    <div class="w3-dropdown-hover">
-      <button class="w3-button">Bus Application
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="w3-dropdown-content w3-bar-block">
-        <a href="apply-book.php" class="w3-bar-item w3-button">New Application</a>
-        <a href="bookhistory.php" class="w3-bar-item w3-button">Booking History</a>
-        <a href="bookhistory1.php" class="w3-bar-item w3-button">Booking History 1</a>
-      </div>
-    </div>
-    <a href="vehicle.php" class="w3-bar-item w3-button">Type Vehicle</a>
-    <a href="logout.php" class="w3-bar-item w3-button">Logout</a>
   </div>
 
 
